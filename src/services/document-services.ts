11074c674d44a7b2e7e772d49d36733b21b9c0db
raphauy <rapha.uy@rapha.uy>
2024-04-11 16:06:02 -0300

    revalidate path on api

--- src/services/document-services.ts
+++ src/services/document-services.ts
@@ -64,7 +64,7 @@ export async function getDocumentsDAOByClient(clientId: string) {
       clientId
     },
     orderBy: {
-      createdAt: 'desc'
+      createdAt: 'asc'
     },
     include: {
       client: true,
